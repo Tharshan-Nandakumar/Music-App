2e4d005d0aa25d7ed0a6f04f22e23a38bb88c939
Tharshan <tharshan1465@gmail.com>
2024-06-24 21:16:37 +0100

    login.js

--- src/Login.js
+++ src/Login.js
@@ -3,8 +3,12 @@ import "./Login.css";
 
 function Login() {
   return (
-    <div className="Login">
-      <p>hi</p>
+    <div className="login">
+      <img
+        src="https://music-b26f.kxcdn.com/wp-content/uploads/2017/06/635963274692858859903160895_spotify-logo-horizontal-black.jpg"
+        alt="Spotify logo"
+      />
+      <a href="#">LOGIN WITH SPOTIFY</a>
     </div>
   );
 }
